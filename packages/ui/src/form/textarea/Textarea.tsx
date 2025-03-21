7e9566feaed9a08cc79fe852c995478622e7e9fe
Farzad Wahabzada <imfrzd@gmail.com>
2025-03-21 18:28:23 +0000

    auth0 removed

--- packages/ui/src/form/textarea/Textarea.tsx
+++ packages/ui/src/form/textarea/Textarea.tsx
@@ -11,7 +11,7 @@ export const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
       <textarea
         ref={ref}
         className={twMerge(
-          "text-grayscale-700 dark:text-grayscale-200 placeholder-grayscale-400 dark:placeholder-grayscale-400 border-grayscale-150 dark:border-grayscale-700 dark:hover:border-grayscale-650 hover:border-grayscale-300 focus:border-grayscale-300 dark:focus:border-grayscale-650 w-full rounded-2xl border-1 px-4 py-2 text-lg focus:outline-none",
+          "text-grayscale-700 dark:text-grayscale-200 placeholder-grayscale-400 dark:placeholder-grayscale-400 border-grayscale-150 dark:border-grayscale-700 dark:hover:border-grayscale-650 hover:border-grayscale-300 focus:border-grayscale-300 dark:focus:border-grayscale-650 w-full rounded-2xl border-1 px-4 py-2 text-lg focus:outline-none disabled:cursor-not-allowed",
           className
         )}
         {...props}
